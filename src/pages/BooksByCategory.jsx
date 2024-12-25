0ff2621def67beeb455d16b5678f4bc0882147ae
NissanJK <jawadul.karim78@gmail.com>
2024-12-25 22:22:54 +0600

    back button added in bookbycategory

--- src/pages/BooksByCategory.jsx
+++ src/pages/BooksByCategory.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import ReactStars from 'react-rating-stars-component';
 import useAxiosSecure from '../hooks/useAxiosSecure';
 import { Helmet } from 'react-helmet';
@@ -10,7 +10,7 @@ const BooksByCategory = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const axiosSecure = useAxiosSecure();
-
+    const navigate = useNavigate();
     useEffect(() => {
         setLoading(true);
         setError(null);
@@ -35,25 +35,30 @@ const BooksByCategory = () => {
             <Helmet>
                 <title>BookHaven | Books on {category}</title>
             </Helmet>
+            <div className='w-11/12 mx-auto mb-8'>
+            <button onClick={() => navigate(-1)} className="btn btn-outline">
+                Back
+            </button>
+            </div>
             <h1 className="text-3xl font-bold mb-8 text-center text-gray-100">Books in {category}</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-11/12 mx-auto">
                 {books.map((book) => (
                     <div key={book._id} className="bg-gray-600 card p-2 rounded-lg shadow hover:shadow-lg transition flex flex-col justify-between"
-                >
-                    <img src={book.image} alt={book.name} className="w-full h-40 object-cover rounded gloss" 
-                    />
-                    <div className="flex-grow space-y-2">
-                        <h2 className="text-xl font-bold mt-2 text-gray-100">{book.name}</h2>
-                        <p className="text-sm text-gray-300">Author: {book.authorName}</p>
-                        <p className="text-sm text-gray-300">Category: {book.category}</p>
-                        <p className="text-sm text-gray-300">Quantity: {book.quantity}</p>
-                        <ReactStars count={5} value={book.rating} size={24} edit={false} />
+                    >
+                        <img src={book.image} alt={book.name} className="w-full h-40 object-cover rounded gloss"
+                        />
+                        <div className="flex-grow space-y-2">
+                            <h2 className="text-xl font-bold mt-2 text-gray-100">{book.name}</h2>
+                            <p className="text-sm text-gray-300">Author: {book.authorName}</p>
+                            <p className="text-sm text-gray-300">Category: {book.category}</p>
+                            <p className="text-sm text-gray-300">Quantity: {book.quantity}</p>
+                            <ReactStars count={5} value={book.rating} size={24} edit={false} />
+                        </div>
+                        <Link to={`/books/${book._id}`} className="btn btn-secondary mt-4 ">
+                            Details
+                        </Link>
                     </div>
-                    <Link to={`/books/${book._id}`} className="btn btn-secondary mt-4 ">
-                        Details
-                    </Link>
-                </div>
-                
+
                 ))}
             </div>
         </div>
